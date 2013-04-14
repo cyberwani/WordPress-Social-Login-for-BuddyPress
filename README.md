f7918be7b73e0c3951e98c86f3f9f5e2069a430e
Nick <nick@shapingcloud.com>
2013-04-14 14:55:22 +0100

    The BuddyPress hack/fix

--- README.md
+++ README.md
@@ -4,6 +4,7 @@ Wordpress social login hacked/fixed to work with BuddyPress.
 I did not create this plugin, all i did was:
 
 1) Made it work correctly with Buddypress
+
 2) Fixed an issue where social imported images would overwite a profile image that a user had uploaded.
 
 The plugin author can be found here: 
--- includes/services/wsl.user.avatar.php
+++ includes/services/wsl.user.avatar.php
@@ -21,17 +21,18 @@ if ( !defined( 'ABSPATH' ) ) exit;
  */
 function wsl_user_custom_avatar($avatar, $mixed, $size, $default, $alt)
 {
+
 	//Check if avatars are enabled
 	if( get_option ( 'wsl_settings_users_avatars' ) )
 	{
 		//Current comment
 		global $comment;
-
+	
 		//Chosen user
 		$user_id = null;
 
 		//Check if we have an user identifier
-		if (is_numeric ($mixed))
+		if (is_numeric ($mixed) || $mixed['item_id'] )
 		{
 			if ($mixed > 0)
 			{
@@ -59,11 +60,51 @@ function wsl_user_custom_avatar($avatar, $mixed, $size, $default, $alt)
 
 		//User found?
 		if ( $user_id )
-		{
-			$user_thumbnail = get_user_meta ( $user_id, 'wsl_user_image', true );
+		{	
 
-			if ( $user_thumbnail )
-			{
+			/** 
+			*
+			* BUDDYPRESS IMAGE FIX 
+			* For anyone who uses the BuddyPress plugin: 
+			* ------------------------------------------
+			* This social login plugin will not display the profile images 
+			* that it had pulled from the social networks the following is a fix for this
+			* 
+			*/
+
+			//Check if user has an uploaded image, if so then return that avatar
+			if(function_exists(bp_get_user_has_avatar))
+				if(bp_get_user_has_avatar($user_id['item_id'])) return($avatar);
+
+			//If an item ID is found that means the wordpress installation is using buddypress and the 
+			//above functions havn't been called, the item_id is the user_id
+			if(is_numeric($user_id['item_id'])){
+
+				// Apply the user_id 
+				$user_id = $user_id['item_id'];
+				
+				// The $size parameter isn't passed through with BuddyPress so the images don't have any size attributes.. 
+				// But it is available in the $avatar parameters image makup, lets extract that value.
+				preg_match("/width=\"(.*?)\"/", $avatar,$matches);
+
+				// Apply the image size
+				$size = $matches[1];
+			}
+
+			/* 
+			* 
+			* The base variables are overwritten when BuddyPress is active, this should now work. 
+			* 
+			*/
+
+			
+			$user_thumbnail = get_user_meta ( $user_id, 'wsl_user_image', true );
+			if ( $user_thumbnail ){
+
+				//Twitter image size fix
+				if($size > 50) $user_thumbnail = str_replace("_normal","",$user_thumbnail);
+
+				//Return image
 				return '<img src="' . $user_thumbnail . '" class="avatar avatar-wordpress-social-login avatar-' . $size . ' photo" height="' . $size . '" width="' . $size . '" />'; 
 			}
 		}
@@ -72,6 +113,20 @@ function wsl_user_custom_avatar($avatar, $mixed, $size, $default, $alt)
 	return $avatar;
 }
 
-add_filter( 'get_avatar', 'wsl_user_custom_avatar', 10, 5 );
+/*
+*
+* BuddyPress uses a different avatar function to grap profile images, so we'll need to hook to that function
+* if buddypress is active.
+*
+* Before the below was edited it was just 
+* add_filter( 'get_avatar', 'wsl_user_custom_avatar', 10, 5 );
+* 
+*/
+
+if(function_exists("bp_core_fetch_avatar"))
+	add_filter( 'bp_core_fetch_avatar', 'wsl_user_custom_avatar', 10, 5 );
+else
+	add_filter( 'get_avatar', 'wsl_user_custom_avatar', 10, 5 );
+
 
 // --------------------------------------------------------------------
